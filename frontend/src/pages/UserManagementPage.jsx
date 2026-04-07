985df841a473b2145337dee6eedab2e067bd91b3
Vishal Bainade <vishal.bainade@nexintelai.com>
2026-04-07 15:03:47 +0530

    refactor(rbac): uncomment support tickets tab in UserManagementPage

--- frontend/src/pages/UserManagementPage.jsx
+++ frontend/src/pages/UserManagementPage.jsx
@@ -69,7 +69,7 @@ const UserManagementPage = () => {
                     Analytics
                   </button>
                 )}
-                {/* {canManageSupport && (
+                {canManageSupport && (
                   <button
                     className={`whitespace-nowrap border-b-2 px-1 py-4 text-sm font-semibold transition ${
                       activeTab === 'Support Tickets'
@@ -78,9 +78,9 @@ const UserManagementPage = () => {
                     }`}
                     onClick={() => setActiveTab('Support Tickets')}
                   >
-                    Support Tickets
+                    
                   </button>
-                )} */}
+                )}
               </nav>
             </div>
 
