9d18d988e9c1aee23ac7bdd7ebabcab52f6b0a44
Vishal Bainade <vishal.bainade@nexintelai.com>
2026-07-17 13:30:45 +0530

    Untrack Frontend/ (superseded by frontend/)

--- Frontend/src/components/DraftComponents/index.js
+++
@@ -1,5 +0,0 @@
-export { default as DraftSelectionCard } from './DraftSelectionCard';
-export { default as GoogleDocsEditor } from './GoogleDocsEditor';
-export { default as MicrosoftWordEditor } from './MicrosoftWordEditor';
-
-
--- Frontend/src/template_drafting_component/store/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Template Drafting Component - Stores Barrel Export
- */
-
-export { useTemplateStore, useFilteredTemplates, useTemplateCategories } from './templateStore';
-export { useUiStore } from './uiStore';
-export { useDraftStore } from './draftStore';
